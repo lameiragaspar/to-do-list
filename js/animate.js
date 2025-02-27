7469fd383cfd3d009e2ceb1f08d70f5868f7b922
Pedro Lameira <120721002+lameiragaspar@users.noreply.github.com>
2025-02-27 22:52:16 +0100

    Loading 2.0

--- js/animate.js
+++ js/animate.js
@@ -1,36 +1,39 @@
 const caixa_dialogo = (dialogo) =>{
-
     if(dialogo == "Vasio"){ //ajude e usuário a preencher todos os dado
-        mostrarCaixaDialogo(`<p>Preencha todos os dados!</p>`)//Chama a função mostrar conteúdo e o parámetro é a mensagem para o usuário
+        carregamento();
+        setTimeout(() =>{
+            mostrarCaixaDialogo(`<p>Preencha todos os dados!</p>`)//Chama a função mostrar conteúdo e o parámetro é a mensagem para o usuário
+        }, 1500)
     }else{
         return
     }
 }
+const focus = document.getElementById("focus");
 
-const mostrarCaixaDialogo = (conteudo) =>{
-    const focus = document.getElementById("focus");
-    const caixaDdialogo = document.getElementById("caixaDdialogo");
+function carregamento(){
     const loading = document.getElementById("loading")
-    const txt = document.getElementById("txt")
-    const fechar_caixa = document.getElementById("fechar_caixa")
-
-    focus.classList.add("focus");
-    fechar_caixa.classList.add("hidden");
     loading.classList.remove("hidden");
-
+    focus.classList.add("focus");
     setTimeout(() =>{
         loading.classList.add("hidden");
-        fechar_caixa.classList.remove("hidden")
-        caixaDdialogo.classList.remove("hidden"); //A class hidden é removida da tag com o id caixaDdialogo, fazendo o elemento aparecer na tela
-        txt.innerHTML = conteudo;
+        focus.classList.remove("focus")
     }, 1500)
+}
+
+const mostrarCaixaDialogo = (conteudo) =>{
+    const caixaDdialogo = document.getElementById("caixaDdialogo");
+    const txt = document.getElementById("txt")
+
+    focus.classList.add("focus");
+    caixaDdialogo.classList.remove("hidden"); //A class hidden é removida da tag com o id caixaDdialogo, fazendo o elemento aparecer na tela
+    txt.innerHTML = conteudo;
 
     function fecharDialogo(){//Responsável por add novamente a class hidden e esconder o elemento anteriormente renderizado
         caixaDdialogo.classList.add("hidden");
         focus.classList.remove("focus")
     }
 
-    fechar_caixa.addEventListener("click", fecharDialogo)//O botão executa a função fecharDialogo
+    document.getElementById("fechar_caixa").addEventListener("click", fecharDialogo)//O botão executa a função fecharDialogo
 }
 
-export default caixa_dialogo;
+export {caixa_dialogo, carregamento};
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import caixa_dialogo from "./animate.js";
+import {caixa_dialogo, carregamento} from "./animate.js";
 
 const num_td = 6;
 
@@ -16,74 +16,72 @@ const add_tarefa = (event)=>{
         caixa_dialogo(parametro)
         return; // Sai da função se algum campo estiver vazio
     }
-    if(tarefa.trim() != "" || prioridade.trim() != "" || descricao.trim()!=""){
-        const loading = document.getElementById("loading")
-        loading.classList.remove("hidden");
-        setTimeout(() =>{
-            loading.classList.add("hidden");
+    if(tarefa.trim() !== "" || prioridade.trim() !== "" || descricao.trim()!==""){
+        carregamento();
+        setTimeout(()=>{
+            //DATA ACTUAL - CRIAÇÃO DA TAREFA
+            const data = new Date()
+            let hoje = (`${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`)
+            //ICONES DE INFORMAÇÃO, EDIÇÃO E DELETE
+            let info = document.createElement("img")
+            info.setAttribute('src',"icon/info.svg")
+            info.setAttribute('alt',"eddite")
+
+            let edite = document.createElement("img")
+            edite.setAttribute('src',"icon/editar.svg")
+            edite.setAttribute('alt',"eddite")
+
+            let delet = document.createElement("img")
+            delet.setAttribute('src',"icon/delete.svg")
+            delet.setAttribute('alt',"eddite")
+            //Criando a linha da tabela
+            let nova_tarefa = document.createElement("tr") //Criação de uma nova linha
+            let nova_td
+            for(let td = 0; td < num_td; td++){
+                nova_td = document.createElement("td")
+
+                switch (td){
+                    case 0:
+                        nova_td.appendChild(info)
+                        break
+                    case 1:
+                        nova_td.textContent= tarefa
+                        break
+                    case 2:
+                        nova_td.textContent = prioridade
+                        if(prioridade == "Alta"){
+                            nova_td.style.backgroundColor ="#ff5252";
+                        }else if(prioridade =="Média"){
+                            nova_td.style.backgroundColor ="#ffa726";
+                        }else if(prioridade =="Baixa"){
+                            nova_td.style.backgroundColor ="#4caf50";
+                    }
+                        break
+                    case 3:
+                        nova_td.textContent = hoje
+                        break
+                    case 4:
+                        nova_td.appendChild(edite)
+                        break
+                    case 5:
+                        nova_td.appendChild(delet)
+                        break
+                    default:
+                        break
+                }
+                nova_tarefa.appendChild(nova_td)
+            }
+
+            //Mostrando a tarefa
+            let tbody = document.getElementById("mostrar_tarefa")
+            tbody.appendChild(nova_tarefa)
+
+            //LIMPANDO OS CAMPOS DE INPUT
+            tarefa = document.getElementById("id_tarefa").value ="";
+            prioridade = document.getElementById("id_prioridade").value = "";
+            descricao = document.getElementById("id_descricao").value = "";
         }, 1500)
     }
-    //DATA ACTUAL - CRIAÇÃO DA TAREFA
-    const data = new Date()
-    let hoje = (`${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`)
-    //ICONES DE INFORMAÇÃO, EDIÇÃO E DELETE
-    let info = document.createElement("img")
-    info.setAttribute('src',"icon/info.svg")
-    info.setAttribute('alt',"eddite")
-
-    let edite = document.createElement("img")
-    edite.setAttribute('src',"icon/editar.svg")
-    edite.setAttribute('alt',"eddite")
-
-    let delet = document.createElement("img")
-    delet.setAttribute('src',"icon/delete.svg")
-    delet.setAttribute('alt',"eddite")
-    //Criando a linha da tabela
-    let nova_tarefa = document.createElement("tr") //Criação de uma nova linha
-    let nova_td
-    for(let td = 0; td < num_td; td++){
-        nova_td = document.createElement("td")
-
-        switch (td){
-            case 0:
-                nova_td.appendChild(info)
-                break
-            case 1:
-                nova_td.textContent= tarefa
-                break
-            case 2:
-                nova_td.textContent = prioridade
-                if(prioridade == "Alta"){
-                    nova_td.style.backgroundColor ="#ff5252";
-                }else if(prioridade =="Média"){
-                     nova_td.style.backgroundColor ="#ffa726";
-                }else if(prioridade =="Baixa"){
-                    nova_td.style.backgroundColor ="#4caf50";
-               }
-                break
-            case 3:
-                nova_td.textContent = hoje
-                break
-            case 4:
-                nova_td.appendChild(edite)
-                break
-            case 5:
-                nova_td.appendChild(delet)
-                break
-            default:
-                break
-        }
-        nova_tarefa.appendChild(nova_td)
-    }
-
-    //Mostrando a tarefa
-    let tbody = document.getElementById("mostrar_tarefa")
-    tbody.appendChild(nova_tarefa)
-
-    //LIMPANDO OS CAMPOS DE INPUT
-    tarefa = document.getElementById("id_tarefa").value ="";
-    prioridade = document.getElementById("id_prioridade").value = "";
-    descricao = document.getElementById("id_descricao").value = "";
 }
 //Se sertificando de que a função add_tarefa é chamada. Se não no HTML, então diretamente no JS
 const adiciona_tarefa = document.getElementById("add_tarefa");
